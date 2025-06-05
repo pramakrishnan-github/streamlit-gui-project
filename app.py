7741bcc7b7fbb3ba98946509c4ed22fa7be4b5ef
pramakrishnan-github <166032272+pramakrishnan-github@users.noreply.github.com>
2025-06-05 10:18:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 # Dropdown options (example lists, replace these with actual values)
-portfolio_options = ["example_portfolio_country"]
-cluster_options = ["example_country_wind", "example_country_solar"]
+portfolio_options = ["example_portfolio_country", "renpro_portfolio_pl"]
+cluster_options = ["example_country_wind", "example_country_solar", "renpro_pl_wind", "renpro_pl_solar"]
 
 # Dropdown selection widgets
 portfolio_slug = st.selectbox("Portfolio Slug", portfolio_options)
